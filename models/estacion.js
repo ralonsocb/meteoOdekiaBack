edd2c487df608bb5e7125262e366eb90020c93cc
ralonsocb <roberto.alonso@mayconsulting.es>
2020-03-12 13:07:53 +0100

    Modified estacion model

--- models/estacion.js
+++ models/estacion.js
@@ -4,6 +4,16 @@ var Schema = mongoose.Schema;
 var estacionSchema = new Schema({
     nombre: { type: String, required: [true, 'El nombre es necesario'] },
     usuario: { type: Schema.Types.ObjectId, ref: 'Usuario' },
+    temperatura: { type: Number},
+    precipitaciones: { type: Number},
+    humedad: { type: Number},
+    velViento: { type: Number},
+    dirViento: { type: Number},
+    brillo: { type: Number},
+    uv: {type: Number},
+    presion: {type: Number},
+    posSol: {type: Number},
+    radiacion: {type: Number},
 }, { collection: 'estaciones' });
 
 module.exports = mongoose.model('Estacion', estacionSchema);
